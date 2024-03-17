a117768733cf508cfc46b2f2c84e23700f81cb12
Mikhail Chernyshov <chernyshov.michaele@icloud.com>
2024-03-17 19:37:15 +0100

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Mikhail Chernyshov on 17/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
