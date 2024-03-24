b3579bb12cd076cb4192e3f42c728a65357b3248
Mikhail Chernyshov <chernyshov.michaele@icloud.com>
2024-03-24 13:41:21 +0100

    More coins downloaded. Clearing project from prints

--- Crypto/Services/CoinImageService.swift
+++ Crypto/Services/CoinImageService.swift
@@ -27,10 +27,8 @@ class CoinImageService {
     private func getCoinImage () {
         if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
             image = savedImage
-            print("Image from File Manager")
         } else {
             downloadCoinImage()
-            print("Downloading image now")
         }
     }
     
