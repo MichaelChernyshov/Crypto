9ef7c465c87c194c1ca2cc0f3ef79cd16d9e29b6
Mikhail Chernyshov <chernyshov.michaele@icloud.com>
2024-03-24 13:38:13 +0100

    Added saving future for the images. In case if it's not saved - downloading.

--- Crypto/Services/CoinImageService.swift
+++ Crypto/Services/CoinImageService.swift
@@ -14,13 +14,27 @@ class CoinImageService {
     
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coins_images"
+    private let imageName: String
     
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
-    private func getCoinImage() {
+    private func getCoinImage () {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print("Image from File Manager")
+        } else {
+            downloadCoinImage()
+            print("Downloading image now")
+        }
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(string: coin.image) else {return}
         
         imageSubscription = NetworkingManager.download(url: url)
@@ -28,8 +42,11 @@ class CoinImageService {
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedImage in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self,
+                let downloadedImage = returnedImage else { return }
+                self.image = returnedImage
+                self.imageSubscription?.cancel()
+                self .fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self. folderName)
             })
 
     }
