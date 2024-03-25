3d2cea8b15b2842be21958b94cc14ea83f3c910a
Mikhail Chernyshov <chernyshov.michaele@icloud.com>
2024-03-25 17:53:39 +0100

    Searchbar with filtering

--- Crypto/Core/Home/ViewModels/HomeViewModel.swift
+++ Crypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -12,6 +12,7 @@ class HomeViewModel: ObservableObject {
     
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
+    @Published var searchText: String = ""
     
     private let dataService = CoinDataService()
     private var cancelables = Set<AnyCancellable>()
@@ -21,7 +22,21 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubsribers() {
-        dataService.$allCoins
+        // Updates all coins and filter
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .map { (text, startingCoins) -> [CoinModel] in
+                guard !text.isEmpty else {
+                    return startingCoins
+                }
+                
+                let lowercasedText = text.lowercased()
+                return startingCoins.filter { coin -> Bool in
+                    return coin.name.lowercased().contains(lowercasedText) ||
+                    coin.symbol.lowercased().contains(lowercasedText) ||
+                    coin.id.lowercased().contains(lowercasedText)
+                }
+            }
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
