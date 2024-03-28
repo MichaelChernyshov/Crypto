6cc894373ca257fd49fe584b459f0fac7c75cf62
Mikhail Chernyshov <chernyshov.michaele@icloud.com>
2024-03-28 15:50:15 +0100

    Added Coin Logo View

---
+++ Crypto/Core/Home/Views/PortfolioView.swift
@@ -0,0 +1,18 @@
+//
+//  PortfolioView.swift
+//  Crypto
+//
+//  Created by Mikhail Chernyshov on 28/03/2024.
+//
+
+import SwiftUI
+
+struct PortfolioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PortfolioView()
+}
