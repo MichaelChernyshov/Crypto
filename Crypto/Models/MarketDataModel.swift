73bc008e230f91a70ff9906461d6ea9627876d94
Mikhail Chernyshov <chernyshov.michaele@icloud.com>
2024-03-28 12:13:07 +0100

    Replaced hardcoded statistics with dynamic data. Extension on dobles. Marketdata service and model created. Renamed dataService to CoinDataS

---
+++ Crypto/Models/MarketDataModel.swift
@@ -0,0 +1,51 @@
+//
+//  MarketDataModel.swift
+//  Crypto
+//
+//  Created by Mikhail Chernyshov on 27/03/2024.
+//
+
+import Foundation
+
+struct GlobalData: Codable {
+    let data: MarketDataModel?
+}
+
+struct MarketDataModel: Codable {
+    let totalMarketCap, totalVolume, marketCapPercentage: [String: Double]
+    let marketCapChangePercentage24HUsd: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case totalMarketCap = "total_market_cap"
+        case totalVolume = "total_volume"
+        case marketCapPercentage = "market_cap_percentage"
+        case marketCapChangePercentage24HUsd = "market_cap_change_percentage_24h_usd"
+    }
+    
+    var marketCap: String {
+        if let item = totalMarketCap.first(where: { (key, value) in
+            return key == "usd"
+        }) {
+            return "$" + item.value.formattedWithAbbreviations()
+        }
+        return ""
+    }
+    
+    var volume: String {
+        if let item = totalVolume.first(where: { (key, value) in
+            return key == "usd"
+        }) {
+            return "$" + item.value.formattedWithAbbreviations()
+        }
+        return ""
+    }
+    
+    var btcDominance: String {
+        if let item = marketCapPercentage.first(where: { (key: String, value: Double) in
+            return key == "btc"
+        }) {
+            return item.value.asPercentString()
+        }
+        return ""
+    }
+}
